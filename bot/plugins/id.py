f18ee66bed74083c9744b72898f2f420d65aad51
reejit <70316178+reejit@users.noreply.github.com>
2021-03-26 20:39:42 +0530

    Create id.py

---
+++ bot/plugins/id.py
@@ -0,0 +1,12 @@
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram import Message as message
+
+@Client.on_message(filters.command(["id"]) & (filters.private | filters.group))
+async def showid(client, message):
+
+        user_id = message.chat.id
+        await message.reply_text(
+            f"Your ID : `{user_id}`",
+            parse_mode="markdown"
+        )
